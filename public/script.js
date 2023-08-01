4633b3431b44c94ad0f1c9e818b5e1debf0b43ff
bentike <ajadiyusuf73@gmail.com>
2023-08-01 18:30:52 +0100

    removed console from script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
 let hamburger = document.getElementById("hamburger");
 let mobileNav = document.getElementById("mobile");
 hamburger.addEventListener("click", function(){
-    console.log(mobileNav.classList);
     mobileNav.classList.toggle("hidden");
 })

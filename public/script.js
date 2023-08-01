ad82a28f0b4983cb2bfc15dff0b72d7344c8c980
bentike <ajadiyusuf73@gmail.com>
2023-08-01 20:30:27 +0100

    added smooth scroll with jQuery

--- public/script.js
+++ public/script.js
@@ -2,4 +2,10 @@ let hamburger = document.getElementById("hamburger");
 let mobileNav = document.getElementById("mobile");
 hamburger.addEventListener("click", function(){
     mobileNav.classList.toggle("hidden");
-})
+});
+
+$('a').click(function (e) {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000);
+});

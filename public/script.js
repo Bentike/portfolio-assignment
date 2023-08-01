49136c75ade5c5e150836bcaf352e46f49387cb0
bentike <ajadiyusuf73@gmail.com>
2023-08-01 17:48:31 +0100

    added toggle to hambugger

--- public/script.js
+++ public/script.js
@@ -1,1 +1,6 @@
-alert("connected");
+let hamburger = document.getElementById("hamburger");
+let mobileNav = document.getElementById("mobile");
+hamburger.addEventListener("click", function(){
+    console.log(mobileNav.classList);
+    mobileNav.classList.toggle("hidden");
+})
